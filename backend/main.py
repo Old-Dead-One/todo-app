699de155e192be8432548bd43f70b7d71b67c86d
Alan McGraw <alanmcgraw@proton.me>
2024-04-19 10:26:13 -0600

    Create main, req, and models

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 
+from models import Todo
+
 app = FastAPI()
 
+todo: list[Todo] = []
 
 
 @app.get("/")
-async def get_todos():
-    return "Hello world"
+async def get_todos() -> list[Todo]:
+    return todo
+
+@app.post("/")
+async def create_todo():
+    return
+
+
---
+++ backend/models.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    number: int
+    description: str
+
