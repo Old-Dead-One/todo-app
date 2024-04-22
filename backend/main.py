c8454bdb9243d3e63a288f81269272eb40ba19d9
Alan McGraw <alanmcgraw@proton.me>
2024-04-22 09:19:32 -0600

    Add main.py models.py etc...

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
-from models import Todo
+
+import models, schemas, crud
+
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-todo: list[Todo] = []
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+@app.get("/", response_model=list[schemas.Todo])
+async def get_todos(db: Session = Depends(get_db)) -> list[schemas.Todo]:
+    return crud.get_todos(db)
 
-@app.get("/")
-async def get_todos() -> list[Todo]:
-    return todo
-
-@app.post("/")
-async def create_todo():
-    return
+@app.post("/", response_model=schemas.Todo)
+async def create_todo(todos: schemas.TodoCreate, db: Session = Depends(get_db)) -> schemas.Todo:
+    return crud.create_todos(db=db, todo=todos)
 
 
